1aeb5586404624ab27462fedbe69e8d0dbe34242
khushraj17 <khushrajrane@gmail.com>
2026-06-02 20:36:54 +0530

    add spam classifier

---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+import sklearn
+ps = PorterStemmer()
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectoizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
+
+st.title("SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button("Predict"):
+    # 1. Preprocess
+    transformed_sms = transform_text(input_sms)
+
+    # 2. Vectorize
+    vector_input = tfidf.transform([transformed_sms])
+
+    # 3. Predict
+    result = model.predict(vector_input)[0]
+
+    # 4. Display result
+    if result == 1:
+        st.error("This is a spam message.")
+    else:
+        st.success("This is not a spam message.")
+
